0b41932df7bb0d07a8717bb475aac030916a7326
Prabhat Gupta <136306864+prabhatam@users.noreply.github.com>
2023-12-18 11:47:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById("button");
+
+const randomcolor = () => {
+    let val = "0123456789ABCDEF";
+    let cons = "#";
+    for(let i=0; i<6; i++)
+    {
+        cons = cons + val[Math.floor(Math.random()*16)];
+    }
+    return cons;
+};
+
+function colorRandomChanger(){
+    document.body.style.backgroundColor = randomcolor();
+}
+
+btn.addEventListener("click", colorRandomChanger);
